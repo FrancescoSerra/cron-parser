7e5142dbf18b5af559ac9621a8500ecbece29f53
Francesco Serra <francesco.serra@ynap.com>
2020-03-10 16:38:01 +0000

    To increase number of cases generated per test

--- src/test/scala/cron/parser/Generators.scala
+++ src/test/scala/cron/parser/Generators.scala
@@ -1,5 +1,14 @@
 package cron.parser
 
-object Generators {
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.Gen._
 
+object Generators {
+  val rangeGenerator: Gen[Range] = for {
+    start <- choose(Int.MinValue, Int.MaxValue)
+    end <- choose(Int.MinValue, Int.MaxValue)
+    maybeStep <- option(choose(Int.MinValue, Int.MaxValue))
+  } yield Range(start,end,maybeStep)
+
+  implicit val arbitraryRange: Arbitrary[Range] = Arbitrary(rangeGenerator)
 }
--- src/test/scala/cron/parser/RegExesTest.scala
+++ src/test/scala/cron/parser/RegExesTest.scala
@@ -3,17 +3,19 @@ package cron.parser
 import RegExes._
 import org.scalatest.{FlatSpec, Matchers}
 import org.scalatest.prop.PropertyChecks
+import Generators._
 
 class RegExesTest extends FlatSpec with Matchers with PropertyChecks {
+  val withinHundred : Int => Boolean = i => 0 <= i && i <= 99
   "validentry" should "match an entry or reject anything else" in forAll { i: Int =>
-    if(i >= 0 && i <= 99)
+    if(withinHundred(i))
       validEntry.matches(i.toString) shouldBe true
     else
       validEntry.matches(i.toString) shouldBe false
   }
 
   "validAsterisk" should "match an asterisk with or without valid step or reject anything else" in forAll { i: Int =>
-    if(i >= 0 && i <= 99) {
+    if(withinHundred(i)) {
       validAsterisk.matches("*") shouldBe true
       validAsterisk.matches(s"*/$i") shouldBe true
     } else {
@@ -23,18 +25,40 @@ class RegExesTest extends FlatSpec with Matchers with PropertyChecks {
 
   }
 
-  "validRange" should "match a range with or without valid step or reject anything else" in forAll { (start: Int, finish: Int, maybeStep: Int) =>
-    if(List(start, finish, maybeStep).forall(i => i >= 0 && i <= 99)) {
-        validRange.matches(s"$start-$finish") shouldBe true
-        validRange.matches(s"$start-$finish/$maybeStep") shouldBe true
-    } else {
-      validRange.matches(s"$start-$finish") shouldBe false
-      validRange.matches(s"$start-$finish/$maybeStep") shouldBe false
+  "validRange" should "match a range with or without valid step or reject anything else" in forAll { range: Range =>
+    val Range(start, finish, maybeStep) = range
+    if(List(start, finish).forall(withinHundred) && maybeStep.forall(withinHundred))
+      validRange.matches(range.toString) shouldBe true
+    else
+      validRange.matches(range.toString) shouldBe false
+  }
+
+  "validList" should "match a list of entries or reject anything else" in forAll { list: List[Int] =>
+    whenever(list.nonEmpty) {
+      if (list.forall(withinHundred))
+        validList.matches(list.mkString(",")) shouldBe true
+      else
+        validList.matches(list.mkString(",")) shouldBe false
     }
   }
 
-  "validList" should "match a list of entries or reject anything else" in forAll {
-    
+  "validListOfRanges" should "match a list of ranges and entries or reject anything else" in forAll { (entries: List[Int], ranges: List[Range]) =>
+    whenever(entries.nonEmpty && ranges.nonEmpty) {
+
+      val entriesFirst = entries.zip(ranges).mkString(",")
+      val rangesFirst = ranges.zip(entries).mkString(",")
+
+      println(entriesFirst)
+      println(rangesFirst)
+      if (entries.forall(withinHundred) &&
+        ranges.forall { case Range(a,b,c) => withinHundred(a) && withinHundred(b) && c.forall(withinHundred) } ) {
+        validListOfRanges.matches(entriesFirst) shouldBe true
+        validListOfRanges.matches(rangesFirst) shouldBe true
+      } else {
+        validListOfRanges.matches(entriesFirst) shouldBe false
+        validListOfRanges.matches(rangesFirst) shouldBe false
+      }
+    }
   }
 
 }
