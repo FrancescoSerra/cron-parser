1334b50f0d208f20bdbded423bab127d8061ee1c
Francesco Serra <fserra@hotels.com>
2020-05-05 23:02:03 +0100

    almost complete full test coverage

--- src/test/scala/cron/parser/MainClassTest.scala
+++ src/test/scala/cron/parser/MainClassTest.scala
@@ -3,12 +3,51 @@ package cron.parser
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import cats.syntax.option._
+import cats.syntax.either._
+import cats.data.NonEmptyChain
+import cats.effect.{ExitCode, IO}
 
 class MainClassTest extends AnyFlatSpec with Matchers {
   val main = new MainClass
 
-  "MainClass" should "store the first argument in optInput" in {
-    main.main(Array("foo"))
-    main.optInput shouldBe "foo".some
+  "MainClass.main" should "return an error string if no argument is passed to it" in {
+    main.mainFunc(None) shouldBe "Please provide a cron line to interpret".asLeft
+  }
+
+  it should "return a valid list of tuples if a valid argument is passed in" in {
+    main.mainFunc("25 6 * * * root /usr/sbin/anacron".some) shouldBe
+      NonEmptyChain(
+        ("minute", Minute(List(25))),
+        ("hour", Hour(List(6))),
+        ("day of month", DayOfMonth((1 to 31).toList)),
+        ("month", Month((1 to 12).toList)),
+        ("day of week", DayOfWeek((0 to 7).toList)),
+        ("command", Command("root /usr/sbin/anacron"))
+      ).asRight
+  }
+
+  it should "return an error if an invalid argument is passed in" in {
+    main.mainFunc("invalid".some) shouldBe
+      "input couldn't be parsed into the expected tokens: invalid".asLeft
+  }
+
+  "MainClass.printRes" should "print the right output if the input is a Right" in {
+    main.printRes(Right(NonEmptyChain(("minute", Minute(List(1)))))).unsafeRunSync shouldBe
+      SuccessResult
+  }
+
+  it should "print a failure if the input is Left" in {
+    main.printRes(Left("gahh")).unsafeRunSync shouldBe
+      ErrorResult
+  }
+
+  "MainClass.run" should "exit with success if correct input was passed" in {
+    main.run(List("25 6 * * * root /usr/sbin/anacron")).unsafeRunSync shouldBe
+      ExitCode.Success
+  }
+
+  it should "exit with failure if invalid input was passed" in {
+    main.run(List("bogus")).unsafeRunSync shouldBe
+      ExitCode.Error
   }
 }
