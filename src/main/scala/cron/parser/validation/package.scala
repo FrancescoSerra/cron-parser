a20da5013a42e7a9527cfe9af098a7ec3e7557d2
Francesco Serra <fn.serra@gmail.com>
2020-06-09 22:00:59 +0100

    more cosmetic changes

--- src/main/scala/cron/parser/validation/package.scala
+++ src/main/scala/cron/parser/validation/package.scala
@@ -1,6 +1,6 @@
 package cron.parser
 
-import cats.data.Kleisli
+import cats.data.{Kleisli, ValidatedNec}
 import cats.implicits._
 import RegExes._
 
@@ -14,35 +14,34 @@ package object validation {
   * Either context which represents a failure in recognising the type of FieldType, or the FieldType itself
   * */
   val validateFormat: Result[String,FieldType] = Kleisli { field =>
-    def validateIn(input: String): Either[InvalidFormat,FieldType] = {
-      input match {
-        case validEntry(entry) => Right(Entry(entry.toInt))
-        case validAsterisk(step) =>
-          Option[String](step).map(_.toInt) match {
-            case some@Some(s) if s > 0 => Right(Asterisk(some))
-            case Some(s) => Left(InvalidFormat(s"*/$s is not in a valid cron field format"))
-            case _ => Right(Asterisk(None))
-          }
-        case validRange(start, end, step) =>
-          val initVal = start.toInt
-          val endVal = end.toInt
-          if (endVal > initVal && (step != "0")) Right(Range(initVal, endVal, Option[String](step).map(_.toInt)))
-          else Left(InvalidFormat(s"Invalid range $initVal-$endVal${Option(step).map(v => s"/$v").getOrElse("")}"))
-        case validList(list) => Right(ListOfEntries(list.split(",").toList.map(e => Entry(e.toInt))))
-        case validListOfRanges(listOfRanges) =>
-          listOfRanges.split(",").toList.collect(validateIn(_)).map {
-            case Right(rangeable: Rangeable) => rangeable.validNec[InvalidFormat]
-            case Right(other) => other.invalidNec[Rangeable]
-            case Left(invalidFormat) => invalidFormat.invalidNec[Rangeable]
-          }.sequence.toEither.map(ListOfRanges).leftMap(invalid => InvalidFormat(invalid.toList.mkString))
-        case validDay(day) => Right(LiteralDay(day))
-        case validMonth(month) => Right(LiteralMonth(month))
-        case token => Left(InvalidFormat(s"$token is not in a valid cron field format"))
-      }
+    lazy val validateIn:  String => Either[InvalidFormat,FieldType] = {
+      case validEntry(entry) => Entry(entry.toInt).asRight
+      case validAsterisk(step) =>
+        Option[String](step).map(_.toInt) match {
+          case some@Some(s) if s > 0 => Asterisk(some).asRight
+          case Some(s) => InvalidFormat(s"*/$s is not in a valid cron field format").asLeft
+          case _ => Asterisk(None).asRight
+        }
+      case validRange(start, end, step) =>
+        val initVal = start.toInt
+        val endVal = end.toInt
+        if (endVal > initVal && (step != "0")) Range(initVal, endVal, Option[String](step).map(_.toInt)).asRight
+        else InvalidFormat(s"Invalid range $initVal-$endVal${Option(step).map(v => s"/$v").getOrElse("")}").asLeft
+      case validList(list) => ListOfEntries(list.split(",").toList.map(e => Entry(e.toInt))).asRight
+      case validListOfRanges(listOfRanges) =>
+        listOfRanges.split(",").toList.collect(validateIn(_)).traverse {
+          case Right(rangeable: Rangeable) => rangeable.validNec[InvalidFormat]
+          case Right(other) => other.invalidNec[Rangeable]
+          case Left(invalidFormat) => invalidFormat.invalidNec[Rangeable]
+        }.toEither.map(ListOfRanges).leftMap(invalid => InvalidFormat(invalid.toList.mkString))
+      case validDay(day) => LiteralDay(day).asRight
+      case validMonth(month) => LiteralMonth(month).asRight
+      case token => InvalidFormat(s"$token is not in a valid cron field format").asLeft
     }
 
     validateIn(field)
   }
+
 /*
 * takes:
 *   - min: Int, the lower bound of the set of possible values for that cron field
@@ -54,18 +53,18 @@ package object validation {
 * */
   def validate[A: Buildable](min: Int, max: Int, stringRepr: String): ValidatedTo[A] = Kleisli { field =>
     (field match {
-      case Entry(value) if value >= min && value <= max => Right(List(value))
+      case Entry(value) if value >= min && value <= max => List(value).asRight
       case Asterisk(maybeStep) =>
-        Right(maybeStep.map(step => (min to max).toList.filter(a => a % step == 0 || a == 0)).getOrElse((min to max).toList))
+        maybeStep.map(step => (min to max).toList.filter(a => a % step == 0 || a == 0)).getOrElse((min to max).toList).asRight
       case Range(start, end, maybeStep) if start >= min && end <= max =>
-        Right(maybeStep.map(step => (start to end).toList.filter(a => a % step == 0 || a == 0)).getOrElse((start to end).toList))
+        maybeStep.map(step => (start to end).toList.filter(a => a % step == 0 || a == 0)).getOrElse((start to end).toList).asRight
       case ListOfEntries(entries) if entries.map(_.value).forall(e => e >= min && e <= max) =>
-        Right(entries.map(_.value).distinct.sorted)
+        entries.map(_.value).distinct.sorted.asRight
       case ListOfRanges(ranges) if ranges.forall(r => r.start >= min && r.end <= max) =>
-        Right(ranges.flatMap(range =>
+        ranges.flatMap(range =>
           range.maybeStep.map(step => (range.start to range.end).toList.filter(a => a % step == 0 || a == 0)).getOrElse((range.start to range.end).toList)
-        ).distinct.sorted)
-      case illegalValue => Left(IllegalValue(s"$illegalValue is not a valid $stringRepr value"))
+        ).distinct.sorted.asRight
+      case illegalValue => IllegalValue(s"$illegalValue is not a valid $stringRepr value").asLeft
     }).map(Buildable[A].build)
   }
 
@@ -73,15 +72,15 @@ package object validation {
   /*
   * validates the minute field length
   * */
-  val validateMinuteLength: Result[FieldType,Minute] = validate[Minute](0,59,"minute")
+  val validateMinuteLength: Result[FieldType,Minute] = validate(0,59,"minute")
 
   /*
   * validates the hour field length
   * */
-  val validateHourLength: Result[FieldType,Hour] = validate[Hour](0,23,"hour")
+  val validateHourLength: Result[FieldType,Hour] = validate(0,23,"hour")
 
   /*
   * validates the day of month field
   * */
-  val validateDayOfMonth: Result[FieldType,DayOfMonth] = validate[DayOfMonth](1,31,"day of month")
+  val validateDayOfMonth: Result[FieldType,DayOfMonth] = validate(1,31,"day of month")
 }
